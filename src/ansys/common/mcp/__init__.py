2e4596e1049546702b4b074a8c652c8f18c35b35
clatapie <78221213+clatapie@users.noreply.github.com>
2025-11-25 15:51:23 +0100

    fix: cleanup

--- src/ansys/common/mcp/__init__.py
+++ src/ansys/common/mcp/__init__.py
@@ -9,10 +9,6 @@ __version__ = "0.0.1"
 from ansys.common.mcp.context import PyAnsysBaseAppContext
 from ansys.common.mcp.helpers import PersistentPythonSession
 from ansys.common.mcp.server import PyAnsysBaseMCP
-from ansys.common.mcp.tools import (
-    check_package_version,
-    get_python_environment_info,
-)
 
 __all__ = [
     "PyAnsysBaseAppContext",
