d494f8c7b38d78f7fb0e994819d4e268044c856a
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2026-03-18 12:36:39 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,18 +73,11 @@ extensions = [
     "sphinx.ext.extlinks",
     "sphinx.ext.graphviz",
     "sphinx.ext.intersphinx",
-    "sphinxcontrib.mermaid",
 ]
 
 # Intersphinx mapping
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    # "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
-    # "numpy": ("https://numpy.org/devdocs", None),
-    # "matplotlib": ("https://matplotlib.org/stable", None),
-    # "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
-    # "pyvista": ("https://docs.pyvista.org/version/stable/", None),
-    # "grpc": ("https://grpc.github.io/grpc/python/", None),
 }
 
 # numpydoc configuration
